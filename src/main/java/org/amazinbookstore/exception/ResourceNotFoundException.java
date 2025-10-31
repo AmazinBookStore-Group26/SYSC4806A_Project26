2fab96146834bc674628b06bc8e13c3a5135eb1f
hajarassim <hajar.assim@gmail.com>
2025-10-30 23:34:28 -0400

    (feat): adds exception type for cart

---
+++ src/main/java/org/amazinbookstore/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.amazinbookstore.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
